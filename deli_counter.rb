2557c013bcf4cb0c5925cc37648b053b162768c3
Zach <shaw.zacht@gmail.com>
2022-05-11 15:54:47 -0700

    commit tree e72596840131371bde90b432d819325023962389

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+    if katz_deli.length == 0
+        puts "The line is currently empty."
+    else 
+        queue = katz_deli.map.with_index do |name, index|
+                " #{index + 1}. #{name}"
+        end
+        puts "The line is currently:#{queue.join}"
+    end
+end
+
+def take_a_number(katz_deli, name) 
+    katz_deli.push(name)
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving (katz_deli)
+    if katz_deli.length > 0
+        puts "Currently serving #{katz_deli[0]}."
+        katz_deli.shift
+    else 
+        puts "There is nobody waiting to be served!"
+    end
+end
